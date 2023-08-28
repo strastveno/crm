fac441ba056964028e9cc9e1341d380dc782c38f
mileticaleksa99 <mileticaleksa99@gmail.com>
2023-08-28 21:28:19 +0200

    kreirana pocetna stranica

---
+++ src/Pocetna.jsx
@@ -0,0 +1,25 @@
+import React from 'react'; 
+
+const Pocetna = () => {
+    return (
+        <div className="crm-container">
+            <h1>Šta je CRM?</h1>
+            <p>
+                CRM (Customer Relationship Management) predstavlja pristup upravljanju odnosima sa vašim kupcima. To je strategija koja se koristi za bolje razumijevanje, privlačenje i zadržavanje kupaca kroz analiziranje i ispravno upravljanje informacijama i interakcijama sa njima.
+            </p>
+            <h2>Čemu služi CRM?</h2>
+            <ul>
+                <li>Upravljanje kontaktima i interakcijama s klijentima.</li>
+                <li>Automatizacija prodaje, marketinga i usluge korisnicima.</li>
+                <li>Segmentacija klijenata i ciljanje određenih tržišnih segmenata.</li>
+                <li>Analiziranje prodajnih podataka i praćenje performansi.</li>
+                <li>Poboljšanje komunikacije unutar tima.</li>
+            </ul>
+            <p>
+                CRM rešenja olakšavaju firmama praćenje i upravljanje interakcijama s klijentima, omogućavajući efikasniju i personalizovaniju komunikaciju.
+            </p>
+        </div>
+    );
+}
+
+export default Pocetna;
