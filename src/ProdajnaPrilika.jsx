2b851c6894301f16b71041da0d00503ba5a30a5a
mileticaleksa99 <mileticaleksa99@gmail.com>
2023-08-29 01:08:48 +0200

    dodat api za konverziju valuta

--- src/ProdajnaPrilika.jsx
+++ src/ProdajnaPrilika.jsx
@@ -1,11 +1,23 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function ProdajnaPrilika({ prilike }) {
     const [filteredPrilike, setFilteredPrilike] = useState([]);
+    const [conversionRates, setConversionRates] = useState({});
+
+    useEffect(() => {
+   
+        axios.get('https://api.exchangerate-api.com/v4/latest/USD')
+        .then(response => {
+            setConversionRates(response.data.rates);
+        })
+        .catch(error => {
+            console.error("Greška prilikom povlačenja kurseva:", error);
+        });
+    }, []);
 
     useEffect(() => {
         const prodavacId = sessionStorage.getItem('id');
-
         const filtriranePrilike = prilike.filter(prilika => prilika.prodavac_id === Number(prodavacId));
         setFilteredPrilike(filtriranePrilike);
     }, [prilike]);
@@ -17,7 +29,9 @@ function ProdajnaPrilika({ prilike }) {
                     <tr>
                         <th>ID</th>
                         <th>Napomena</th>
-                        <th>Očekivani Iznos</th>
+                        <th>Očekivani Iznos (USD)</th>
+                        <th>Očekivani Iznos (RSD)</th>
+                        <th>Očekivani Iznos (EUR)</th>
                         <th>Status</th>
                         <th>Ime Klijenta</th>
                         <th>Email Klijenta</th>
@@ -25,11 +39,13 @@ function ProdajnaPrilika({ prilike }) {
                     </tr>
                 </thead>
                 <tbody>
-                    { filteredPrilike && filteredPrilike.map(prilika => (
+                    {filteredPrilike.map(prilika => (
                         <tr key={prilika.id}>
                             <td>{prilika.id}</td>
                             <td>{prilika.napomena}</td>
                             <td>{prilika.ocekivaniIznos}</td>
+                            <td>{(prilika.ocekivaniIznos * conversionRates.RSD).toFixed(2)}</td>
+                            <td>{(prilika.ocekivaniIznos * conversionRates.EUR).toFixed(2)}</td>
                             <td>{prilika.status}</td>
                             <td>{prilika.client.name}</td>
                             <td>{prilika.client.email}</td>
